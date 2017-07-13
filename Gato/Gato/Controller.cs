babe5b813c25d0c7ffb399340a240b00e1616bd3
Kevin <pkevin.walsh@gmail.com>
2017-07-13 12:41:01 -0600

    Se declaran las clases (Solo tienen implementado los constructores) para seguir el patron MVC

---
+++ Gato/Gato/Controller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gato
+{
+    class Controller
+    {
+        public Controller(Model model, View view)
+        {
+            
+        }
+    }
+}
