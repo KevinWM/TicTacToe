33e5f1341ba91e47264ba8f9c0baef222bb7a211
Kevin <pkevin.walsh@gmail.com>
2017-07-13 15:31:59 -0600

    Se crear varios metodos para mostrar informacion inicial y dibujar el tablero de juego

--- Gato/Gato/View.cs
+++ Gato/Gato/View.cs
@@ -8,9 +8,33 @@ namespace Gato
 {
     class View
     {
+        Model model;
+
         public View(Model model)
         {
-
+            this.model = model;
         }
+
+        public void ImprimirMensajeInicial()
+        {
+            Console.Title = ("Tic Tac Toe Version 2");
+            Console.WriteLine("Welcome to Tic Tac Toe.\n");
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadLine();
+            Console.Clear();
+        }
+
+        public void dibujarTablero(String[] tablero,String jugador)
+        {
+            Console.Clear();
+            Console.WriteLine("Jugador: " + jugador);
+            Console.WriteLine("Seleccione el numero de la posicion donde desea poner la ficha");
+            for (int i = 0; i < 7; i += 3)
+            {
+                Console.WriteLine(tablero[i] + "|" + tablero[i + 1] + "|" + tablero[i + 2]);
+            }
+        
+        }
+
     }
 }
