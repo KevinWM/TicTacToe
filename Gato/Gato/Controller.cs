0bcfa1e31d942618ced4715457b6f4dd88f34607
Kevin <pkevin.walsh@gmail.com>
2017-07-13 15:28:37 -0600

    Se agregaron los metodos para mostrar el mensaje inicial del juego, verificar si el juego no ha terminado y el de jugar un turno

--- Gato/Gato/Controller.cs
+++ Gato/Gato/Controller.cs
@@ -8,9 +8,33 @@ namespace Gato
 {
     class Controller
     {
+        View view;
+        Model model;
+
         public Controller(Model model, View view)
         {
             
+            this.model = model;
+            this.view = view;
+        }
+
+        public void mensajeInicialConsola()
+        {
+            this.view.ImprimirMensajeInicial();
+        }
+
+        public Boolean verificarSiElJuegoContinua()
+        {
+            return this.model.verificarSiElJuegoContinua();
+        }
+
+        public void jugarUnTurno() 
+        {
+            while (true)
+            {
+                this.view.dibujarTablero(this.model.tablero, "X");
+                int selection = Convert.ToInt32(Console.ReadLine());
+            }
         }
     }
 }
